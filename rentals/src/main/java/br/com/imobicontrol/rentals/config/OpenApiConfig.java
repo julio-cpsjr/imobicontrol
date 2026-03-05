878b3ab6d9fd973863823f110985ea14ec221212
julio-cpsjr <juliocpsjunior@gmail.com>
2026-03-05 09:41:05 -0300

    docs(api): implement api config

---
+++ rentals/src/main/java/br/com/imobicontrol/rentals/config/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package br.com.imobicontrol.rentals.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI rentalsOpenAPI() {
+        return new OpenAPI().info(
+                new Info()
+                        .title("Rentals API")
+                        .description("My Rentals API")
+                        .version("v1.0"));
+    }
+}
